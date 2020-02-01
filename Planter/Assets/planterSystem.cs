f834f2e2328e3b1972ffd3e9f54a0c6e102b17dc
Shawn Fernandes <43974678+sf0912@users.noreply.github.com>
2020-02-01 14:24:14 -0600

    Add files via upload

---
+++ Planter/Assets/planterSystem.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class planterSystem : MonoBehaviour
+{
+  //  public Vector3 spawnHere;
+    
+    public GameObject seed;
+    public GameObject plantLocation;
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.name=="plantLocation")
+        {
+         
+            Instantiate(seed,plantLocation.transform.position,Quaternion.identity);
+        }
+    }
+
+    
+}
+
