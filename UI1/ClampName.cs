46dd2d5b1034ec74d1f5e3d418a0c0193b11086e
Shawn Fernandes <43974678+sf0912@users.noreply.github.com>
2020-02-02 13:28:11 -0600

    Add files via upload

---
+++ UI1/ClampName.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ClampName : MonoBehaviour
+{
+    public Text nameLabel;
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 namePos = Camera.main.WorldToScreenPoint(this.transform.position);
+        nameLabel.transform.position = namePos;
+    }
+}
