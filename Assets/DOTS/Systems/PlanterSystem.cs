e0c8cc26f04ba7f25042c275df81be28b9566cd7
rasam550 <rasam_hossain@yahoo.com>
2020-02-05 22:38:15 -0600

    Fixed Seed Display system

--- Assets/DOTS/Systems/PlanterSystem.cs
+++ Assets/DOTS/Systems/PlanterSystem.cs
@@ -24,13 +24,16 @@ public class PlanterSystem : MonoBehaviour
     {
         if (collision.transform.tag == "plantLocation")
         {
+            if(Settings.GetSeedCount() > 0)
+            {
+                // Instantiate(seed, collision.transform.position, Quaternion.identity);
+                SpawnTree(collision.transform.position);
+                collision.gameObject.tag = "planted";
+                Destroy(collision.gameObject);
+                Settings.RemoveSeed();
+                SoundController.Instance.Play3D(SoundController.Instance.GetRandomClip(SoundController.Instance.planterClips), collision.transform.position);
+            }
             
-            // Instantiate(seed, collision.transform.position, Quaternion.identity);
-            SpawnTree(collision.transform.position);
-            collision.gameObject.tag = "planted";
-            Destroy(collision.gameObject);
-            Settings.RemoveSeed();
-            SoundController.Instance.Play3D(SoundController.Instance.GetRandomClip(SoundController.Instance.planterClips), collision.transform.position);
         }
     }
 
