f9b694c588496a73c3273a66e3839a4416c2e175
Rene Dufour-Contreras <rene.cpp@gmail.com>
2020-02-05 23:45:18 -0600

    MovedSeed UI to Planter

---
+++ Assets/Scripts/SeedCanvasControl.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SeedCanvasControl : MonoBehaviour
+{
+    Camera mainCamera;
+
+    void Start()
+    {
+        mainCamera = Camera.main;
+    }
+
+    
+    void Update()
+    {
+        transform.LookAt(mainCamera.transform);
+    }
+}
